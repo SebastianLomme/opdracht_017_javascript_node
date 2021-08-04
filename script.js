f2266569919458ce0c0b38c7785a0f4e2397368b
SebastianLomme <77025210+SebastianLomme@users.noreply.github.com>
2021-08-04 21:46:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("Hallo Winc Academy studenten")
+
+// //dit is een grote somzh
+
+// let sum1 = 1230941 * 1823794
+// console.log(sum1)
+// let sum2 = 637263/54
+// console.log(sum2)
+console.log("test")
